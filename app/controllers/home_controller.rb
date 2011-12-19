8aa2fe6afaa5d6fb617e8e6dcad55fef1c22ad04
Aleksey Kurepin <a.kurepin@gmail.com>
2011-12-19 20:36:44 +0400

    many change views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  layout :puffer_pages
+
   def index
+    @news = News.all :limit => 3, :order => 'created_at DESC'
+    @posts = Post.all :limit => 10, :order => 'created_at DESC'
   end
 
 end
