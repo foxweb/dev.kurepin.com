c19eb271a368a51279bfb684157648f54bb9b16c
Aleksey Kurepin <lesha@kurepin.com>
2012-06-09 16:32:11 +0400

    fix Home puffer_pages render

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
-  layout :puffer_pages
 
   def index
     @news = News.all :limit => 3, :order => 'created_at DESC'
     @posts = Post.all :limit => 10, :order => 'created_at DESC'
+    render :puffer_page => '/'
   end
 
 end
