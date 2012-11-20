a0fe594f1f0ebd6ed97e4e1f02033f448381c7c4
Aleksey Kurepin <lesha@kurepin.com>
2012-11-20 14:11:55 +0400

    Clearance config added

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,5 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'donotreply@example.com'
+  config.cookie_expiration = lambda { 1.month.from_now }
+  config.mailer_sender = 'lesha@kurepin.com'
+  config.user_model = User
 end
