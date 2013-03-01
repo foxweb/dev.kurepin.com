eb3385ba96c91e16854e51b45e20c98999dfc6e8
Aleksey Kurepin <lesha@kurepin.com>
2013-03-01 18:55:10 +0400

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,48 @@
-# -*- encoding: utf-8 -*-
-set :rvm_ruby_string, "1.9.3@dev"
+# encoding: utf-8
+set :rvm_ruby_string, '1.9.3@dev'
 set :rvm_install_type, :stable
 set :rvm_install_shell, :zsh
-require "rvm/capistrano"
+require 'rvm/capistrano'
 
 default_run_options[:pty] = true
 
-set :application, "dev.kurepin.com"
-set :repository,  "git@github.com:foxweb/dev.kurepin.com.git"
+set :application, 'dev.kurepin.com'
 
-set :scm, :git
-set :user, "foxweb"
+set :user, 'foxweb'
 set :use_sudo, false
 set :deploy_to, "/home/foxweb/work/deploy/#{application}"
 set :deploy_via, :remote_cache
 
-# role :web, "dev.kurepin.com"
-# role :app, "dev.kurepin.com"
-# role :db,  "dev.kurepin.com", primary: true
+role :web, 'foxweb.dyndns-home.com'
+role :app, 'foxweb.dyndns-home.com'
+role :db,  'foxweb.dyndns-home.com', primary: true
 
-role :web, "foxweb.dyndns-home.com"
-role :app, "foxweb.dyndns-home.com"
-role :db,  "foxweb.dyndns-home.com", primary: true
+set :scm, :git
+set :repository,  'git@github.com:foxweb/dev.kurepin.com.git'
+set :git_enable_submodules, 1
+set :branch, 'master'
 
-set :scm_user, "foxweb"
-set :branch, "master"
 ssh_options[:forward_agent] = true
 
-before "deploy:setup", "rvm:install_rvm"
-
-after "deploy", "deploy:bundle_gems"
-after "deploy:bundle_gems", "deploy:restart"
+before 'deploy:setup', 'rvm:install_rvm'
 
 namespace :deploy do
   task :bundle_gems do
     run "cd #{deploy_to}/current && bundle install"
   end
+
+  task :finalize_update do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/settings.local.yml #{release_path}/config/settings.local.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+    run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
+    run "ln -nfs #{shared_path}/log #{release_path}/log"
+  end
+
+  task :precompile_assets do
+    run "cd #{release_path}; bundle exec rake assets:precompile RAILS_ENV=production"
+  end
+
   task :start do ; end
   task :stop do ; end
   task :restart, roles: :app, except: { no_release: true } do
